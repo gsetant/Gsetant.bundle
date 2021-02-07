a12f38cf139bf8d224ab3c24e3b5214b0159bbee
chunsi yang <chunsi.yang@gmail.com>
2021-02-07 21:29:02 +1100

    add artist and album

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,19 +1,12 @@
 # -*- coding: utf-8 -*-
-import os
 import base64
 import json
-from io import BytesIO
-from lxml import etree
-from datetime import datetime
 import random
 import string
-import Framework
-import errno
-import shutil
-import stat
-import tempfile
 import sys
-from io import open as iopen
+from datetime import datetime
+
+from lxml import etree
 
 if sys.platform == 'win32':
     import ctypes
@@ -91,7 +84,8 @@ class GsetantForMoviesAgent(Agent.Movies):
             'part_file': Part_file,
             'video_type': Video_type,
             'video_librarySectionID': Video_librarySectionID,
-            'video_librarySectionTitle': Video_librarySectionTitle
+            'video_librarySectionTitle': Video_librarySectionTitle,
+            'media_type': 'movie'
         }
 
         result = None
@@ -187,6 +181,171 @@ class GsetantForMoviesAgent(Agent.Movies):
 class GsetantForTVShowAgent(Agent.TV_Shows):
     name = NAME + ' TV_Show ' + VERSION
     pass
+
+
+class GsetantForArtist(Agent.Artist):
+    name = name = NAME + ' Artist ' + VERSION
+    languages = [
+        Locale.Language.English,
+        Locale.Language.Chinese
+    ]
+
+    def search(self, results, media, lang, manual):
+        tc = ToolsClass()
+        library_url = 'http://127.0.0.1:32400/library/metadata/%s' % media.id
+        base_result = HTTP.Request(library_url, timeout=timeout).content
+        xml = etree.XML(base_result)
+        Video_librarySectionTitle = xml.xpath(
+            '//MediaContainer/@librarySectionTitle')[0]
+        HTTP.ClearCache()
+        HTTP.CacheTime = CACHE_1MONTH
+        values = {
+            'token': Prefs['Gsetant_token'],
+            'autoFlag': False,
+            'file_name': media.filename,
+            'artist': media.artist,
+            'album': media.album,
+            'track': media.track,
+            'index': media.index,
+            'video_librarySectionTitle': Video_librarySectionTitle,
+            'media_type': 'artist'
+        }
+        logi(media)
+        logi(media.track)
+
+        result = None
+        try:
+            result = HTTP.Request(
+                '%s:%s/scan' % (tc.convertHttp(Prefs['Gsetant_api_host']), Prefs['Gsetant_api_port']),
+                data=json.dumps(values), timeout=timeout).content
+        except Exception as ex:
+            loge(ex)
+
+        if result is not None:
+            json_data_list = json.loads(result)
+            if json_data_list['state']:
+                data_list = json_data_list['meta_data']
+                data_list.reverse()
+                for json_data in data_list:
+                    for artist_result in json_data.get('artist'):
+                        id = '%s|%s' % (''.join(random.sample(
+                            string.ascii_letters + string.digits, 60)), json.dumps(artist_result))
+                        id = base64.b64encode(id).replace('/', '_')
+                        name = artist_result.get('title')
+                        score = 100
+                        year = '1900-01-01'
+                        thumb = tc.convertHttp('%s:%s%s' % (
+                            Prefs['Gsetant_api_host'], Prefs['Gsetant_api_port'], artist_result.get('poster')))
+                        new_result = dict(id=id, name=name, year=year,
+                                          score=score, lang=lang, thumb=thumb)
+                        results.Append(MetadataSearchResult(**new_result))
+
+    def update(self, metadata, media, lang):
+        tc = ToolsClass()
+        data = base64.b64decode(metadata.id.replace("_", '/'))
+        logi(data)
+        json_data_list = data.split('|')[1]
+        logi(json_data_list)
+        json_data_list = json.loads(json_data_list)
+        metadata.title = json_data_list['title']
+        metadata.summary = json_data_list['summary']
+        for genres in json_data_list['genres'].split(','):
+            metadata.genres.add(genres)
+        metadata.rating = json_data_list['rating']
+        for collection in json_data_list['collections'].split(','):
+            metadata.collections.add(collection)
+        poster_url = tc.convertHttp('%s:%s%s' % (
+            Prefs['Gsetant_api_host'], Prefs['Gsetant_api_port'], json_data_list['poster']))
+        logi('海报缓存:%s' % poster_url)
+        poster = HTTP.Request(poster_url)
+        metadata.posters[poster_url] = Proxy.Media(poster, sort_order=0)
+        art_url = tc.convertHttp('%s:%s%s' % (
+            Prefs['Gsetant_api_host'], Prefs['Gsetant_api_port'], json_data_list['poster']))
+        metadata.posters[art_url] = Proxy.Media(poster, sort_order=0)
+
+
+class GsetantForAlbum(Agent.Album):
+    name = name = NAME + ' Album ' + VERSION
+    languages = [
+        Locale.Language.English,
+        Locale.Language.Chinese
+    ]
+
+    def search(self, results, media, lang, manual):
+        tc = ToolsClass()
+        library_url = 'http://127.0.0.1:32400/library/metadata/%s' % media.id
+        base_result = HTTP.Request(library_url, timeout=timeout).content
+        xml = etree.XML(base_result)
+        Video_librarySectionTitle = xml.xpath(
+            '//MediaContainer/@librarySectionTitle')[0]
+        HTTP.ClearCache()
+        HTTP.CacheTime = CACHE_1MONTH
+        values = {
+            'token': Prefs['Gsetant_token'],
+            'autoFlag': False,
+            'file_name': media.filename,
+            'name': media.name,
+            'artist': media.artist,
+            'album': media.album,
+            'track': media.track,
+            'index': media.index,
+            'video_librarySectionTitle': Video_librarySectionTitle,
+            'media_type': 'album'
+        }
+        logi(media)
+        logi(media.track)
+
+        result = None
+        try:
+            result = HTTP.Request(
+                '%s:%s/scan' % (tc.convertHttp(Prefs['Gsetant_api_host']), Prefs['Gsetant_api_port']),
+                data=json.dumps(values), timeout=timeout).content
+        except Exception as ex:
+            loge(ex)
+
+        if result is not None:
+            json_data_list = json.loads(result)
+            if json_data_list['state']:
+                data_list = json_data_list['meta_data']
+                data_list.reverse()
+                for json_data in data_list:
+                    id = '%s|%s' % (''.join(random.sample(
+                        string.ascii_letters + string.digits, 60)), json.dumps(json_data.get('album')))
+                    id = base64.b64encode(id).replace('/', '_')
+                    name = json_data.get('album').get('title')
+                    score = 100
+                    year = json_data.get('album').get('originally_available_at')
+                    if year == '':
+                        year = '1900-01-01'
+                    thumb = tc.convertHttp('%s:%s%s' % (
+                        Prefs['Gsetant_api_host'], Prefs['Gsetant_api_port'], json_data.get('album').get('poster')))
+                    new_result = dict(id=id, name=name, year=year,
+                                      score=score, lang=lang, thumb=thumb)
+                    results.Append(MetadataSearchResult(**new_result))
+
+    def update(self, metadata, media, lang):
+        tc = ToolsClass()
+        data = base64.b64decode(metadata.id.replace("_", '/'))
+        logi(data)
+        json_data_list = data.split('|')[1]
+        logi(json_data_list)
+        json_data_list = json.loads(json_data_list)
+        metadata.title = json_data_list['title']
+        metadata.summary = json_data_list['summary']
+        metadata.original_title = json_data_list['original_title']
+        metadata.studio = json_data_list['studio']
+        for collection in json_data_list['collections'].split(','):
+            metadata.collections.add(collection)
+        originally_available_at = json_data_list['originally_available_at']
+        if json_data_list['originally_available_at'] == '':
+            originally_available_at = '1900-01-01'
+        metadata.originally_available_at = datetime.strptime(
+            originally_available_at.replace('/', '-'), r'%Y-%m-%d')
+        poster_url = tc.convertHttp('%s:%s%s' % (
+            Prefs['Gsetant_api_host'], Prefs['Gsetant_api_port'], json_data_list['poster']))
+        logi('海报缓存:%s' % poster_url)
+        poster = HTTP.Request(poster_url)
+        metadata.posters[poster_url] = Proxy.Media(poster, sort_order=0)
 
 
 class ToolsClass():
